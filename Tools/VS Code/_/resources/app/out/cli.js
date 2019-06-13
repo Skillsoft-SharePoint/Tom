f4df184d0e017dfd00fa2c8268785b44786d18eb
ThomasCBarkley <36200171+ThomasCBarkley@users.noreply.github.com>
2019-06-13 13:45:41 -0400

    6/13/2019

---
+++ Tools/VS Code/_/resources/app/out/cli.js
@@ -0,0 +1,6 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+"use strict";const bootstrap=require("./bootstrap");bootstrap.avoidMonkeyPatchFromAppInsights(),bootstrap.configurePortable(),bootstrap.enableASARSupport(),require("./bootstrap-amd").load("vs/code/node/cli");
+//# sourceMappingURL=https://ticino.blob.core.windows.net/sourcemaps/c7d83e57cd18f18026a8162d042843bda1bcf21f/core/cli.js.map
---
+++ Tools/VS Code/_/resources/app/out/vs/workbench/contrib/debug/node/telemetryApp.nls.js
@@ -0,0 +1,5 @@
+/*!--------------------------------------------------------
+ * Copyright (C) Microsoft Corporation. All rights reserved.
+ *--------------------------------------------------------*/
+define("vs/workbench/contrib/debug/node/telemetryApp.nls",{"vs/base/node/processes":["Can't execute a shell command on a UNC drive."]});
+//# sourceMappingURL=https://ticino.blob.core.windows.net/sourcemaps/c7d83e57cd18f18026a8162d042843bda1bcf21f/core/vs\workbench\contrib\debug\node\telemetryApp.nls.js.map
