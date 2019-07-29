3c45b7025b635ed536050451423cdb0944390b05
ThomasCBarkley <36200171+ThomasCBarkley@users.noreply.github.com>
2019-07-29 13:12:37 -0400

    update

---
+++ spfx-task-list-web-part-master/src/webparts/spfxTaskListWebPart/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface ISpfxTaskListWebPartWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'SpfxTaskListWebPartWebPartStrings' {
+  const strings: ISpfxTaskListWebPartWebPartStrings;
+  export = strings;
+}
